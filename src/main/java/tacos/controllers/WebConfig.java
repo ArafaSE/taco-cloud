8945a8379f55e38423ba375f7bf2c38bc52b38fb
Mohamed Arafa <mohamed@1-Tech-MohamedArafa.local>
2022-05-14 11:03:04 +0200

    Fix Data loader

--- src/main/java/tacos/controllers/WebConfig.java
+++ src/main/java/tacos/controllers/WebConfig.java
@@ -1,15 +1,44 @@
 package tacos.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import tacos.data.IngredientRepository;
+import tacos.dto.Ingredient;
+import tacos.dto.Ingredient.Type;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer, CommandLineRunner {
+    @Autowired
+    IngredientRepository ingredientRepository;
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("home");
     }
 
+
+    @Override
+    public void run(String... args) throws Exception {
+        dataLoader(ingredientRepository);
+    }
+
+    @Bean
+    public CommandLineRunner dataLoader(IngredientRepository repo) {
+        return args -> {
+            repo.save(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
+            repo.save(new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
+            repo.save(new Ingredient("GRBF", "Ground Beef", Type.PROTEIN));
+            repo.save(new Ingredient("CARN", "Carnitas", Type.PROTEIN));
+            repo.save(new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES));
+            repo.save(new Ingredient("LETC", "Lettuce", Type.VEGGIES));
+            repo.save(new Ingredient("CHED", "Cheddar", Type.CHEESE));
+            repo.save(new Ingredient("JACK", "Monterrey Jack", Type.CHEESE));
+            repo.save(new Ingredient("SLSA", "Salsa", Type.SAUCE));
+            repo.save(new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
+        };
+    }
 }
